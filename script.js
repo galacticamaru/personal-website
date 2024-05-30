d56874f153a46c666a41d228d4a4e48644586690
galacticamaru <50388383+galacticamaru@users.noreply.github.com>
2024-05-30 17:02:59 +1000

    Create script.js and write nav bar

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggle-button').addEventListener('click', function() {
+    document.querySelector('.navbar-links').classList.toggle('active');
+    document.querySelector('.toggle-button').classList.toggle('active');
+});
